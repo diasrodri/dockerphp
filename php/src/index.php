f7ed61a0267207f2897ed4e5465526770313faf6
Rodrigo ALves Dias <diasrodri@gmail.com>
2023-01-18 22:35:45 -0300

    Modifiquei index.php adicionando sql connection

--- php/src/index.php
+++ php/src/index.php
@@ -1,3 +1,20 @@
 <?php
-echo "Hello word!"
+//These are the defined authentication environment in the db service
+
+// The MySQL service named in the docker-compose.yml.
+$host = 'db';
+
+// Database use name
+$user = 'MYSQL_USER';
+
+//database user password
+$pass = 'MYSQL_PASSWORD';
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
 ?>

3c137abf8f3af63a6f9a708215c75856d21472d9
Rodrigo ALves Dias <diasrodri@gmail.com>
2023-04-19 12:01:41 -0300

    V1

--- php/src/control.php
+++ php/src/control.php
@@ -1,14 +1,6 @@
 <?php
-
-$homepage = file_get_contents('web_ctr.txt');
-echo $homepage;
-
-$myfile = fopen("web_ctr.txt", "r") or die("Unable to open file!");
-
-echo $myfile;
-
+$myfile = fopen("web_ctr.txt", "r");
+$contents = fread($myfile, filesize($myfile));
+echo $contents;
 fclose($myfile);
-
-
-
 ?>

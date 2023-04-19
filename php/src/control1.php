6ea2afb5f4c4b9b847ec9bcee04151d046160b91
Rodrigo ALves Dias <diasrodri@gmail.com>
2023-04-19 00:53:40 -0300

    V1

--- php/src/control1.php
+++ php/src/control1.php
@@ -67,33 +67,30 @@ printf("Table myCity successfully created.\n");
 $conn->close();
 
 
-$phpconnect = mysqli_connect($host,$user, $pass);
-if (mysqli_connect_errno())
-{
-echo "Connection Failed; " . mysqli_connect_error();
+
+// Create connection
+$conn = new mysqli($host,$user, $pass, $mydatabase);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-else
-{
-echo "Connection Established.<br>";
- 
-$phpdatabase = "CREATE TABLE MyFriends (
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(20) NOT NULL,
-lastname VARCHAR(20) NOT NULL,
-email VARCHAR(50))";
- 
-if(mysqli_query( $phpconnect,$phpdatabase))
-{  
-echo "Table Created successfully.<br>";  
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
 }
-else
-{  
-echo "Table Creation Failed; ".mysqli_error($phpconnect);  
-}  
-}
- 
-mysqli_close($phpconnect);
-echo "Connection Closed.";
+
+$conn->close();
 
 
 

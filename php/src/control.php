af1474ff065fad37a193e7bb543bb347abe3a058
Rodrigo ALves Dias <diasrodri@gmail.com>
2023-04-19 12:04:49 -0300

    V1

--- php/src/control.php
+++ php/src/control.php
@@ -1,6 +1,10 @@
 <?php
 $filename = "web_ctr.txt";
+echo $filename;
 $handle = fopen($filename, "r");
+echo $handle;
+
 $contents = fread($handle, filesize($filename));
+echo $contents;
 fclose($handle);
 ?>
